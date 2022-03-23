6a65bedf46741dbbe44bb9cb4a833725a65723b8
edgarbp95 <edgarbp95@hotmail.com>
2022-03-23 11:55:18 -0300

    act

--- tic-tac-toe/app.js
+++ tic-tac-toe/app.js
@@ -4,6 +4,8 @@ const restart = document.querySelector(".restart");
 const botonNueva = document.querySelector(".boton_nueva_partida");
 const popUp = document.querySelector(".nueva_partida");
 
+
+//Estos son los posibles patrones ganadores
 let patronesGanadores = [ 
     [0, 1, 2],
     [0, 3, 6],
@@ -15,6 +17,7 @@ let patronesGanadores = [
     [2, 4, 6],
   ];
 
+  //Funcion que activa los botones y resetea los ajustes para una nueva partida. Utilizada en boton Restart y Nueva Partida.
   const activarBotones = ()=>{
       btnRef.forEach((element)=>{
           element.disabled = false;
@@ -25,10 +28,10 @@ let patronesGanadores = [
             btnRef[i].classList.remove("turnoX");
             btnRef[i].classList.remove("turnoO");
         }
-         
       })
   }
 
+  //Funcion que desactiva botones, utilizada al ganar.
   const desactivarBotones = ()=>{
       btnRef.forEach((element)=>{
           element.disabled = true;
@@ -36,12 +39,14 @@ let patronesGanadores = [
       
   }
 
+  //Boton para resetear el tablero y empezar de 0
   restart.addEventListener("click",()=>{
       contador = 0;
       turnoX = true;
       activarBotones();
   })
 
+  //Funcion que se ejecuta al haber empate. Cambia el color y aparece mensaje de que empate
   const esEmpate = ()=>{
       for(let i=0;i<btnRef.length;i++){
           btnRef[i].style.backgroundColor= "hsl(213, 19%, 18%)";
@@ -49,6 +54,7 @@ let patronesGanadores = [
       draw.innerHTML = "¡Es un empate!"
   }
 
+  //Funcion cuando hay ganador. Se desplega POP UP
   const funcionGanadora = (letra)=>{
       desactivarBotones();
       popUp.style.display = "flex";
@@ -64,55 +70,60 @@ let patronesGanadores = [
   let turnoX = true;
   let contador = 0;
 
+
+  //Funcion para recorrer los array de patrones ganadores y verficar coincidencia
   const chequearGanador = ()=>{
     for (ganador of patronesGanadores){
         let [element1,element2,element3] = [
             btnRef[ganador[0]].innerText,
             btnRef[ganador[1]].innerText,
             btnRef[ganador[2]].innerText,
-        ]; console.log(ganador[0])
- 
+        ];  
         if(element1 != "" && element2 != "" && element3 != ""){
             if(element1 == element2 && element2 == element3){
                 funcionGanadora(element1);
             }
         }
- 
     }
   }
 
-   
-
+//Evento de click a todos los cuadrados del tablero.
 btnRef.forEach((element)=>{
     element.addEventListener("click",()=>{
         if(turnoX){
+            //Juega la X
             element.innerText = "X";
             turnoX = false;
             element.disabled = true;
             element.classList.add("turnoX")
         } else{
+            //Juega la O
             element.innerText = "O";
             turnoX = true;
             element.classList.add("turnoO")
             element.disabled = true;
         }
 
+        //Aumenta el contador
         contador++;
 
+        //Si el contador es 9, declaramos empate. (El maximo de jugadas que puede haber sin ganar es 9.)
         if(contador==9){
-            //Es un empate
+            //Llamamos a la funcion de empate, que bloquea la mesa.
             esEmpate();
         }
 
+        //chequemos si hay ganador en cada click
         chequearGanador();
     })
 
 })
 
+//Boton para empezar nueva partida, se encuentra en POP UP que se abre al ganar.
 botonNueva.addEventListener("click",()=>{
     contador = 0;
     turnoX = true;
     activarBotones();
     popUp.style.display = "none";
-    
 })
+
